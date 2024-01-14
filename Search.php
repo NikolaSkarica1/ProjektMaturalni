3f1ef14312d7841b6803da02dcb50524439eb2d1
Skarica <nikola.skarica5@gmail.com>
2024-01-14 20:04:44 +0100

    CSS
    i fucking love css

--- Search.php
+++ Search.php
@@ -1,14 +1,22 @@
 <html>
 <head>
     <title>Search</title>
+    <link rel="stylesheet" href="izgled.css">
 </head>
 <body>
 <?php
+    $unos=$_GET['select'];
+    echo("Resoults for search '".$unos."' <br/>");
     $connection=mysqli_connect("localhost","root","","baza");
-    $select="SELECT * FROM filmovi WHERE title LIKE '%".$_GET['select']."%'";
+    $select="SELECT * FROM filmovi WHERE title LIKE '%".$unos."%'";
     $selected=mysqli_query($connection,$select);
     foreach ($selected as $key => $value) {
-        echo("<img width=250px height=380px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
+        echo("
+        <div id='film'>
+            <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+            <p id='title'>".$value['Title']."</p>
+        </div>  
+        ");
     }
 ?>    
 </body>
--- index.php
+++ index.php
@@ -3,86 +3,143 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="izgled.css">
     <title>Index</title>
 </head>
 <body>
-    <h1>Filmovi</h1>
+    <h1>Filmovi!</h1>
     <form action="Search.php" method="GET">
         Unesite Ime filma:<input type="text" name="select" >
         <input type="submit" value="Upit na bazu">
     </form>
 
-    <h2>Top Rated</h2>
-    <?php
-        $connection=mysqli_connect("localhost","root","","baza");
-        $select="SELECT * FROM `filmovi` ORDER BY `filmovi`.`Vote_Average` DESC LIMIT 8";
-        $selected=mysqli_query($connection,$select);
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
+    <div id="section">
+        <h2>Top Rated</h2>
+        <?php
+            $connection=mysqli_connect("localhost","root","","baza");
+            $select="SELECT * FROM `filmovi` ORDER BY `filmovi`.`Vote_Average` DESC LIMIT 15";
+            $selected=mysqli_query($connection,$select);
+            foreach ($selected as $key => $value) {
+                echo("
+                    <div id='film'>
+                        <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                        <p id='title'>".$value['Title']."</p>
+                    </div>  
+                    ");
+            }
+        ?>
+    </div>
 
-    <h2>Newest</h2>
-    <?php
-        $select="SELECT * FROM `filmovi` ORDER BY `filmovi`.`Relese_date` DESC LIMIT 8";
-        $selected=mysqli_query($connection,$select);
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
+    <div id="section">
+        <h2>Newest</h2>
+        <?php
+            $select="SELECT * FROM `filmovi` ORDER BY `filmovi`.`Relese_date` DESC LIMIT 8";
+            $selected=mysqli_query($connection,$select);
+            foreach ($selected as $key => $value) {
+                echo("
+                <div id='film'>
+                    <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                    <p id='title'>".$value['Title']."</p>
+                </div>  
+                ");
+            }
+        ?>
+        </div>
 
-    <h2>Oldest</h2>
-    <?php
-        $select="SELECT * FROM `filmovi` ORDER BY `filmovi`.`Relese_date` ASC LIMIT 8";
-        $selected=mysqli_query($connection,$select);
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
+    <div id="section">
+        <h2>Oldest</h2>
+        <?php
+            $select="SELECT * FROM `filmovi` ORDER BY `filmovi`.`Relese_date` ASC LIMIT 8";
+            $selected=mysqli_query($connection,$select);
+            foreach ($selected as $key => $value) {
+                echo("
+                <div id='film'>
+                    <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                    <p id='title'>".$value['Title']."</p>
+                </div>  
+                ");
+            }
+        ?>
+    </div>
 
-    <h2>Action-Adventure</h2>
-    <?php
-        $select="SELECT * FROM `filmovi` WHERE genre_id = 28 or 12 and genre_id_2 = 28 or 12 LIMIT 8";
-        $selected=mysqli_query($connection,$select);    
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
+    <div id="section">
+        <h2>Action-Adventure</h2>
+        <?php
+            $select="SELECT * FROM `filmovi` WHERE genre_id = 28 or 12 and genre_id_2 = 28 or 12 LIMIT 8";
+            $selected=mysqli_query($connection,$select);    
+            foreach ($selected as $key => $value) {
+                echo("
+                <div id='film'>
+                    <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                    <p id='title'>".$value['Title']."</p>
+                </div>  
+                ");
+            }
+        ?>
+    </div>
 
-    <h2>Romance</h2>
-    <?php
-        $select="SELECT * FROM `filmovi` WHERE genre_id = 10749 or genre_id_2 = 10749 LIMIT 8";
-        $selected=mysqli_query($connection,$select);
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
+    <div id="section">
+        <h2>Romance</h2>
+        <?php
+            $select="SELECT * FROM `filmovi` WHERE genre_id = 10749 or genre_id_2 = 10749 LIMIT 8";
+            $selected=mysqli_query($connection,$select);
+            foreach ($selected as $key => $value) {
+                echo("
+                <div id='film'>
+                    <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                    <p id='title'>".$value['Title']."</p>
+                </div>  
+                ");
+            }
+        ?>
+    </div>
 
-    <h2>Horror</h2>
-    <?php
-        $select="SELECT * FROM `filmovi` WHERE genre_id = 27 or genre_id_2 = 27 LIMIT 8";
-        $selected=mysqli_query($connection,$select);
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
+    <div id="section">
+        <h2>Horror</h2>
+        <?php
+            $select="SELECT * FROM `filmovi` WHERE genre_id = 27 or genre_id_2 = 27 LIMIT 8";
+            $selected=mysqli_query($connection,$select);
+            foreach ($selected as $key => $value) {
+                echo("
+                <div id='film'>
+                    <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                    <p id='title'>".$value['Title']."</p>
+                </div>  
+                ");
+            }
+        ?>
+    </div>
+    
+    <div id="section">
+        <h2>Fantasy</h2>
+        <?php
+            $select="SELECT * FROM `filmovi` WHERE genre_id = 14 or genre_id_2 = 14 LIMIT 8";
+            $selected=mysqli_query($connection,$select);
+            foreach ($selected as $key => $value) {
+                echo("
+                <div id='film'>
+                    <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                    <p id='title'>".$value['Title']."</p>
+                </div>  
+                ");
+            }
+        ?>
+    </div>
 
-    <h2>Fantasy</h2>
-    <?php
-        $select="SELECT * FROM `filmovi` WHERE genre_id = 14 or genre_id_2 = 14 LIMIT 8";
-        $selected=mysqli_query($connection,$select);
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
-
-    <h2>Sci-Fi</h2>
-    <?php
-        $select="SELECT * FROM `filmovi` WHERE genre_id = 878 or genre_id_2 = 878 LIMIT 8";
-        $selected=mysqli_query($connection,$select);
-        foreach ($selected as $key => $value) {
-            echo("<img width=180px height=260px src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>");
-        }
-    ?>
+    <div id="section">
+        <h2>Sci-Fi</h2>
+        <?php
+            $select="SELECT * FROM `filmovi` WHERE genre_id = 878 or genre_id_2 = 878 LIMIT 8";
+            $selected=mysqli_query($connection,$select);
+            foreach ($selected as $key => $value) {
+                echo("
+                <div id='film'>
+                    <img id='poster-index' src='https://www.themoviedb.org/t/p/w1280/".$value['poster_path']."'></img>
+                    <p id='title'>".$value['Title']."</p>
+                </div>  
+                ");
+            }
+        ?>
+    </div>
 </body>
 </html>
